24031b2fb20a202c74c0c1472d0ba626424a1f19
aelalaoui.ext <aelalaoui.ext@plussimple.com>
2025-10-07 21:31:43 +0200

    update client with type

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -10,6 +10,42 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
+/**
+ * App\Models\Client
+ *
+ * @property string $uuid
+ * @property string|null $user_id
+ * @property string $type
+ * @property string|null $company_name
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $phone
+ * @property string|null $email
+ * @property string|null $address
+ * @property string|null $city
+ * @property string|null $country
+ * @property ClientStatus $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Contract> $contracts
+ * @property-read int|null $contracts_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Alert> $alerts
+ * @property-read int|null $alerts_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Task> $tasks
+ * @property-read int|null $tasks_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Claim> $claims
+ * @property-read int|null $claims_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\File> $files
+ * @property-read int|null $files_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Signature> $signatures
+ * @property-read int|null $signatures_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Communication> $communications
+ * @property-read int|null $communications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Property> $properties
+ * @property-read int|null $properties_count
+ * @property-read string $full_name
+ */
 class Client extends Model
 {
     use HasFactory, HasUuid, HasProperties;
