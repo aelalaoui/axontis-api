862f97ec5558ac1b12c32cb23ba4c1be51e53410
Adil <a.alaoui.2089@gmail.com>
2026-01-01 22:50:58 +0100

    fix migration

--- database/migrations/2026_01_01_193935_add_step_to_clients.php
+++ database/migrations/2026_01_01_193935_add_step_to_clients.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('clients', function (Blueprint $table) {
-            $table->addColumn('string', 'step')
+            $table->string('step')
                 ->after('status')
                 ->nullable();
         });
