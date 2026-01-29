2c4743a8e9cc1ded6d5469a184cd6f28ca91d942
Adil <a.alaoui.2089@gmail.com>
2026-01-29 21:18:44 +0100

    installation to blue

--- resources/js/Pages/Client/Installations/Index.vue
+++ resources/js/Pages/Client/Installations/Index.vue
@@ -104,7 +104,7 @@ const groupedInstallations = computed(() => {
 </script>
 
 <template>
-    <div class="min-h-screen bg-gradient-to-br from-slate-900 via-green-900 to-slate-900">
+    <div class="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-900">
         <Head title="Mes Installations" />
 
         <AppHeader />
