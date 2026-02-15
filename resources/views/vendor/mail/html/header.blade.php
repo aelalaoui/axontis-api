d6ccb49f15f4cc4799e0bf4831c7894ee2b683ad
Adil <a.alaoui.2089@gmail.com>
2026-02-15 13:09:19 +0100

    email blading

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,8 @@
 <tr>
     <td class="header" style="text-align: center; background: rgba(30, 41, 59, 0.8); border-bottom: 1px solid rgba(245, 158, 11, 0.2);">
         <a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
-             <!-- AXONTIS Logo Image - 100% Compatible avec tous les clients email -->
             <img src="{{ asset('images/email-logo-axontis.png') }}"
-                 alt="AXONTIS"
+                 alt="AXONTIS LOGO"
                  width="800"
                  height="200"
                  style="display: block; margin: 0 auto 18px auto;"
